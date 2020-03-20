a86a085090f72c4f8243d376d91ea2a9e014f19b
Yann Lederrey <yann.lederrey@heig-vd.ch>
2020-03-20 13:42:25 +0100

    update script to python3

--- files/manual-decryption.py
+++ files/manual-decryption.py
@@ -12,33 +12,34 @@ __status__ 		= "Prototype"
 
 from scapy.all import *
 import binascii
-import rc4
-
+from rc4 import RC4
 #Cle wep AA:AA:AA:AA:AA
-key='\xaa\xaa\xaa\xaa\xaa'
+key= b'\xaa\xaa\xaa\xaa\xaa'
 
 #lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
 arp = rdpcap('arp.cap')[0]  
 
 # rc4 seed est composé de IV+clé
-seed = arp.iv+key 
+seed = arp.iv+key
 
 # recuperation de icv dans le message (arp.icv) (en chiffre) -- je passe au format "text". Il y a d'autres manières de faire ceci...
-icv_encrypted='{:x}'.format(arp.icv).decode("hex")
+icv_encrypted='{:x}'.format(arp.icv)
 
 # text chiffré y-compris l'icv
-message_encrypted=arp.wepdata+icv_encrypted 
+message_encrypted=arp.wepdata+bytes.fromhex(icv_encrypted)
 
-# déchiffrement avec rc4
-cleartext=rc4.rc4crypt(message_encrypted,seed)  
+# déchiffrement rc4
+cipher = RC4(seed, streaming=False)
+cleartext=cipher.crypt(message_encrypted)
 
 # le ICV est les derniers 4 octets - je le passe en format Long big endian
 icv_enclair=cleartext[-4:]
-(icv_numerique,)=struct.unpack('!L', icv_enclair)
+icv_enclair = icv_enclair
+icv_numerique=struct.unpack('!L', icv_enclair)
 
 # le message sans le ICV
-text_enclair=cleartext[:-4] 
+text_enclair=cleartext[:-4]
 
-print 'Text: ' + text_enclair.encode("hex")
-print 'icv:  ' + icv_enclair.encode("hex")
-print 'icv(num): ' + str(icv_numerique)
+print ('Text: ' + text_enclair.hex())
+print ('icv:  ' + icv_enclair.hex())
+print ('icv(num): ' + str(icv_numerique))
